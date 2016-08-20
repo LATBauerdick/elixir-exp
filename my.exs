89d8388f88598b5aa900df1669540c962e68ca08
LATBauerdick <github@latbauerdick.de>
2016-08-20 09:09:06 -0500

    my.exs works

--- my.exs
+++ my.exs
@@ -1,23 +1,63 @@
-defmodule My do
-  def convert(s, t \\ "./") do
-    do_sys(["echo",
-              "/mnt/P/O/P2016/2016/2016-01/2016-01-2/2016-01-01-DSCF3317.JPG",
-              "-resize", "2560x2048", "-quality", "50",
-              "./2016-01-01-DSCF3317.JPG"
-    ])
-    Path.wildcard(s <>"*.JPG")
+defmodule LATButil do
+
+  def convert(source, target) do
+#
+# recursively convert any .JPG file in the source tree
+# to a smaller JPG in the target tree
+# -- creating directories as required
+# -- not overwriting exiting files
+#
+    dtree(source)
+    |> Enum.map(fn p -> a=Path.relative_to(p, source);
+            cond do
+              a == p -> {source, target}
+              true -> {Path.absname(a, source), Path.absname(a, target)}
+            end
+            end)
+    |> Enum.each(fn {s, t} -> IO.puts "#{s} -> #{t}"
+                  File.mkdir_p(t)
+                  do_convert s, t
+                 end)
+
+  end
+
+  defp do_convert(s, t) do
+    Path.wildcard(Path.absname("*.JPG",s))
     |> Enum.map(&Path.basename/1)
+    |> Enum.filter(fn path -> not File.exists?(Path.absname(path,t)) end)
     |> Enum.map(fn x ->
-          [ "convert", s<>x, "-resize", "2560x2048", "-quality" , "50", t<>x ]
+          [ "convert", Path.absname(x,s), "-resize", "2560x2048",
+          "-quality" , "50", Path.absname(x,t) ]
                 end)
-#    |> Stream.map(fn x -> Task.async(My, :do_cmd, [x]) end)
     |> Stream.map(fn x -> Task.async(fn -> do_sys(x) end) end)
     |> Enum.map(&Task.await/1)
+
   end
   defp do_sys([cmd | args]), do: System.cmd(cmd, args,
                                     into: IO.stream(:stdio, :line))
+
+  defp dtree(dir) do
+    cond do
+      File.dir?(dir) ->
+        [dir | File.ls!(dir)
+          |> Enum.filter(fn name -> File.dir?("#{dir}/#{name}") end)
+          |> Enum.map(fn name -> ["#{dir}/#{name}" | dtree("#{dir}/#{name}")] end)
+          |> List.flatten()
+        ]
+      true -> []
+    end
+  end
+
 end
 
 #      |> Stream.map(​fn​ name -> Task.async(​fn​ -> load_task(name) ​end​) ​end​)
 
-My.convert "/mnt/P/O/P2016/2016/2016-01/2016-01-2/"
+source = "/mnt/P/O/P2016/2016"
+###target = "/Users/bauerdic/Dropbox/Pictures/sJPEGs/2016"
+target = Path.expand("~/2016")
+
+p = "2016-05" # allow to restrict to sub path
+sp = Path.absname(p, source)
+tp = Path.absname(p, target)
+
+LATButil.convert sp, tp
