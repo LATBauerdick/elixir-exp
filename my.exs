1d17e7ea687f440ce8a77292dd277e9d13c10852
LATBauerdick <github@latbauerdick.de>
2016-08-19 21:48:19 -0500

    use tasks to convert JPEGs

--- my.exs
+++ my.exs
@@ -1,12 +1,23 @@
 defmodule My do
   def convert(s, t \\ "./") do
-    {:ok, l} = File.ls(s)
-    l
+    do_sys(["echo",
+              "/mnt/P/O/P2016/2016/2016-01/2016-01-2/2016-01-01-DSCF3317.JPG",
+              "-resize", "2560x2048", "-quality", "50",
+              "./2016-01-01-DSCF3317.JPG"
+    ])
+    Path.wildcard(s <>"*.JPG")
+    |> Enum.map(&Path.basename/1)
     |> Enum.map(fn x ->
-          [ s<>x, "-size 2560x2048", "-quality 50", t<>x] end)
-    |> Enum.map(fn x -> System.cmd("echo", x) end)
-    |> Enum.each(&IO.inspect/1)
+          [ "convert", s<>x, "-resize", "2560x2048", "-quality" , "50", t<>x ]
+                end)
+#    |> Stream.map(fn x -> Task.async(My, :do_cmd, [x]) end)
+    |> Stream.map(fn x -> Task.async(fn -> do_sys(x) end) end)
+    |> Enum.map(&Task.await/1)
   end
+  defp do_sys([cmd | args]), do: System.cmd(cmd, args,
+                                    into: IO.stream(:stdio, :line))
 end
+
+#      |> Stream.map(​fn​ name -> Task.async(​fn​ -> load_task(name) ​end​) ​end​)
 
 My.convert "/mnt/P/O/P2016/2016/2016-01/2016-01-2/"
